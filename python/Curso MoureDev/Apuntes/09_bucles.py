fd4709435295cb7153062965a9b6bdaf54b98dd3
alvarocastilla05 <castilla.caalv24@triana.salesianos.edu>
2025-01-24 09:58:34 +0100

    Mas cosas de python

---
+++ python/Curso MoureDev/Apuntes/09_bucles.py
@@ -0,0 +1,20 @@
+### Bucles ###
+
+# While
+
+my_condition = 0
+
+while my_condition < 10:
+    print(my_condition)
+    my_condition +=2
+else: # Es Opcional
+    print("Mi condición es igual o mayor que 10")
+
+print("La ejecución continúa")
+
+while my_condition < 20:
+    my_condition += 2
+    if my_condition == 15:
+        print("Mi condición es igual a 15")
+
+    print("Mi condición es menor que 20")
